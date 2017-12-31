56d744015ae4369a70e601c737340e1039171eb7
amanYogi <amanrags@gmail.com>
2017-12-31 14:51:14 +0530

    react  bootstrap

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
+// import logo from './logo.svg';
+// import './App.css';
+import FormExample from './LoginPage.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <FormExample />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/LoginPage.js
@@ -0,0 +1,74 @@
+import {FormGroup,ControlLabel,FormControl,HelpBlock,Button,Grid, Row, Col} from 'react-bootstrap';
+import React, { Component } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
+class FormExample extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      value: '',
+    };
+    this.getValidationState = this.getValidationState.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  getValidationState() {
+    const length = this.state.value.length;
+    if (length > 10) return 'success';
+    else if (length > 5) return 'warning';
+    else if (length > 0) return 'error';
+    return null;
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+  }
+
+  render() {
+    return (
+      <form>
+        <FormGroup
+          controlId="formBasicText"
+          validationState={this.getValidationState()}
+        >
+        <Grid>
+          <Row className="show-grid">
+            <Col xs={12} md={8}>
+                        <ControlLabel>Username</ControlLabel>
+                        <FormControl
+                          type="text"
+                          value={this.state.value}
+                          placeholder="Enter text"
+                          onChange={this.handleChange}
+                        />
+            </Col>
+          </Row>
+
+          <Row className="show-grid">
+            <Col xs={6} md={4}>
+              <ControlLabel>Passphrase</ControlLabel>
+              <FormControl
+                type="text"
+                value={this.state.value}
+                placeholder="Enter text"
+                onChange={this.handleChange}
+              />
+            </Col>
+          </Row>
+
+          <Row className="show-grid">
+            <Col xs={6} xsOffset={6}>
+              <Button bsStyle="primary">Primary</Button>
+            </Col>
+          </Row>
+
+        </Grid>
+      </FormGroup>
+    </form>
+    );
+  }
+};
+
+export default FormExample;
